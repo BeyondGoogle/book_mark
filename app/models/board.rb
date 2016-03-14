4b50d1c70bc9b670c0ffc54ccdab6aaa635ff7e3
SoyaTakahashi <appleeeen0525@gmail.com>
2016-03-15 04:03:12 +0900

    [add]add Favorite model

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,5 @@
 class Board < ActiveRecord::Base
   # association
+  has_many :favorites, dependent: :delete_all
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # association
-  has_many :boards
+  has_many :boards, dependent: :delete_all
+  has_many :favorites, dependent: :delete_all
 end

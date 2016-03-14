a469a16461014fdff40dc545437b03c0893faf35
SoyaTakahashi <appleeeen0525@gmail.com>
2016-03-15 04:14:59 +0900

    [clean]refactoring code

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,6 @@
 class Board < ActiveRecord::Base
   # association
-  has_many :board_cards, dependent: :delete_all
-  has_many :favorites, dependent: :delete_all
+  has_many   :board_cards, dependent: :delete_all
+  has_many   :favorites,   dependent: :delete_all
   belongs_to :user
 end

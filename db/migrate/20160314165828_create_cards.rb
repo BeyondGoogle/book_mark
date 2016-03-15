73659f83f2482841fc92fccda9024bdc860b48ed
SoyaTakahashi <appleeeen0525@gmail.com>
2016-03-15 15:41:20 +0900

    [modify]urlにunique制約もたせた

--- db/migrate/20160314165828_create_cards.rb
+++ db/migrate/20160314165828_create_cards.rb
@@ -1,7 +1,7 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
-      t.string    :url
+      t.string    :url, unique: true
       t.string    :title
       t.text      :detail
       t.integer   :bookmarks_count, default: 0

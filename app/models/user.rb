b2add1d849585880c99238a4034ef7921bb7d5e4
SoyaTakahashi <appleeeen0525@gmail.com>
2016-04-23 02:13:37 +0900

    Change user associated method name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # association
-  has_many :bookmarks, dependent: :delete_all
-  has_many :cards,     through: :bookmarks
-  has_many :favorites, dependent: :delete_all
-  has_many :boards,    through: :favorites
-  has_many :boards,    dependent: :delete_all
+  has_many :bookmarks,       dependent: :delete_all
+  has_many :cards,           through: :bookmarks
+  has_many :favorites,       dependent: :delete_all
+  has_many :favorite_boards, through: :favorites, source: :boards
+  has_many :created_boards,  dependent: :delete_all
 end
